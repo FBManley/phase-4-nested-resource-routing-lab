a19c45137db4f51805cc1ff37fa281253010722d
Jake Manley <jbmanley45@gmail.com>
2022-12-11 16:39:25 -0800

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private 
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+  def find_user
+    User.find(params[:user_id])
+  end
+
 
 end
